b23328c9fa04bfacb894a430a57a3c8513a37d10
Jessica Watts <jessperr182@hotmail.com>
2020-09-09 16:50:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma (phrase)
   elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
-    "NO NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
 end
